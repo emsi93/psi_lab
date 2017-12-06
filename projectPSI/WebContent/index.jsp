7adc66766fa81b0ff592d0a29e37608b3917e89c
emsi93 <krejpowiczmichal@gmail.com>
2017-12-06 17:32:45 +0100

    widok formularz logowanie + servlet

--- projectPSI/WebContent/index.jsp
+++ projectPSI/WebContent/index.jsp
@@ -1,10 +1,29 @@
-<%@include file="/includes/header.jsp"%>
+﻿<%@include file="/includes/header.jsp"%>
 <div class="container">
 	<div class="row">
 		<div class="col-lg-3"></div>
 		<div class="col-lg-6">
-			Hello World <a href="/projectPSI/index2.jsp">link</a>
-			<button type="button" class="btn">Basic</button>
+			<h2><label>Logowanie</label></h2></br>
+			<% 
+				if(request.getAttribute("msg") != null){%>
+					<div class="alert alert-danger">
+						<%=  request.getAttribute("msg").toString()%>
+					</div>
+				 	
+				<% }
+			%>
+			<form method="post" action="login">
+			<div class="form-group">
+				<label for="login">Login</label><br/>
+				<input id="login" type="text" name="login" class="form-control"><br/>
+			</div>
+			<div class="form-group">
+				<label for="password">Hasło</label><br/> 
+				<input id="password" type="password" name="password" class="form-control"><br/>
+			</div>
+			<input type="submit" value="Zaloguj" class="btn btn-primary">
+			<input type="reset" value="Wyczyść" class="btn btn-primary">
+			</form>
 		</div>
 		<div class="col-lg-3"></div>
 	</div>
---
+++ projectPSI/src/com/project/psi/servlets/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.project.psi.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
+		String url = null;
+		
+		if((login.equals("") || login==null)  || (password.equals("") || password==null))
+		{
+			url = "/index.jsp";
+			request.setAttribute("msg", "B³êdne dane");
+			
+		}else
+		{
+			url = "/welcome.jsp";
+		}
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+}
